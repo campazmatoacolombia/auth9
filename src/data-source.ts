dfe6206af64354f2d5df0107cfc22ff24e2fe150
campazmatoacolombia <samulfbx@gmail.com>
2026-08-20 16:22:25 -0500

    cambio

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,17 +10,23 @@ export default new DataSource(
     ? {
         type: 'postgres',
         url: process.env.DATABASE_URL,
-        entities: ['src/**/*.entity.ts'],
-        migrations: ['src/migrations/*.ts'],
-        ssl: { rejectUnauthorized: false },
+
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/migrations/*.js'],
+
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }
     : {
         type: 'postgres',
+
         host: process.env.POSTGRES_HOST,
         port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
+
         entities: ['src/**/*.entity.ts'],
         migrations: ['src/migrations/*.ts'],
       },
